9b82aa6432230638aec60de9701876bc3f8e2d24
SadingoDev <m.huch@sadingo.de>
2017-07-24 17:04:35 +0200

    CategoryItem in Provider eingefügt

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -17,6 +17,9 @@ use Plenty\Plugin\ConfigRepository;
 
 class CeresSadingoServiceProvider extends ServiceProvider
 {
+	private static $templateKeyToViewMap = [
+		'tpl.category.item'      => 'Category.Item.CategoryItem',       // provide template to use for item categories
+	];
 	/**
 	 * Register the service provider.
 	 */
