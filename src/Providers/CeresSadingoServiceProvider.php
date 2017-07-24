f8f97b618c5bd04fc140e9addf844ccb247b7782
SadingoDev <m.huch@sadingo.de>
2017-07-24 17:59:17 +0200

    Category dem Provider hinzugefügt

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -18,7 +18,10 @@ use Plenty\Plugin\ConfigRepository;
 class CeresSadingoServiceProvider extends ServiceProvider
 {
 	private static $templateKeyToViewMap = [
+		'tpl.category.content'   => 'Category.Content.CategoryContent', // provide template to use for content categories
 		'tpl.category.item'      => 'Category.Item.CategoryItem',       // provide template to use for item categories
+		'tpl.category.blog'      => 'PageDesign.PageDesign',            // provide template to use for blog categories
+		'tpl.category.container' => 'PageDesign.PageDesign'            // provide template to use for container categories
 	];
 	/**
 	 * Register the service provider.
@@ -33,6 +36,16 @@ class CeresSadingoServiceProvider extends ServiceProvider
         $container->setTemplate("CeresSadingo::Homepage.Homepage");
         return false;
     });
+
+		// provide mapped category IDs - DEPRECATED?
+		$eventDispatcher->listen('init.categories', function (CategoryMap $categoryMap) use (&$config) {
+			$categoryMap->setCategoryMap(array(
+					CategoryKey::HOME => $config->get("CeresSadingo.global.category.home"),
+					CategoryKey::PAGE_NOT_FOUND => $config->get("CeresSadingo.global.category.page_not_found"),
+					CategoryKey::ITEM_NOT_FOUND => $config->get("CeresSadingo.global.category.item_not_found")
+			));
+
+		}, self::EVENT_LISTENER_PRIORITY);
 
 		// footer view
 		$eventDispatcher->listen('IO.init.templates', function(Partial $partial)
