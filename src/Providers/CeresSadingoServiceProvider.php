917e7d1b80b69ab0ff575279767fba5bdac4c6b1
SadingoDev <m.huch@sadingo.de>
2017-07-24 15:25:59 +0200

    CategoryItem hinzugefügt

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -14,11 +14,13 @@ use IO\Helper\ComponentContainer;
 
 class CeresSadingoServiceProvider extends ServiceProvider
 {
+	private static $templateKeyToViewMap = [
+		'tpl.category.item'      => 'Category.Item.CategoryItem',       // provide template to use for item categories
+];
 	/**
 	 * Register the service provider.
 	 */
-	public function register(){
-	}
+	public function register(){}
 
 	public function boot (Twig $twig, Dispatcher $eventDispatcher)
   {
