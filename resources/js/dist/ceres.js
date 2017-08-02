d28e849f40c7c0ae28fb55bb5e5f65093e578031
SadingoDev <m.huch@sadingo.de>
2017-08-02 15:22:33 +0200

    event.target wieder eingefügt

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -626,7 +626,7 @@ Vue.component("add-to-basket", {
                     var widthCart = cartElem.prop('offsetWidth');
                     var heightCart = cartElem.prop('offsetHeight');
 
-                    var imgElem = angular.element(document.getElementsByClassName("active"));
+                    var imgElem = angular.element(event.target.parentNode.parentNode.childNodes[1]);
                     alert(imgElem);
                     var parentElem = angular.element(event.target.parentNode.parentNode);
                     var offsetLeft = imgElem.prop("offsetLeft");
