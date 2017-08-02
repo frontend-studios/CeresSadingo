b4c492dae1180c57af6738da757a616c08996adc
SadingoDev <m.huch@sadingo.de>
2017-08-02 12:24:08 +0200

    script angepasst animation

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -573,7 +573,7 @@ $('.add-to-cart').on('click', function () {
                     'height': '150px',
                     'width': '150px',
                     'z-index': '100'
-
+            })
                 .animate({
                 'top': cart.offset().top + 10,
                     'left': cart.offset().left + 10,
