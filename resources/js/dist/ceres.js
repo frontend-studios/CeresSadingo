ca7a12abfb866098e5197b8b6bab390e478de6c8
SadingoDev <m.huch@sadingo.de>
2017-08-02 11:43:40 +0200

    script angepasst

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -558,45 +558,45 @@ Vue.component("add-item-to-basket-overlay", {
 
 var ResourceService = require("services/ResourceService");
 
-
 $('.add-to-cart').on('click', function () {
-        var cart = $('.shopping-cart');
-        var imgtodrag = $(this).parent('.item').find("img").eq(0);
-        if (imgtodrag) {
-            var imgclone = imgtodrag.clone()
-                .offset({
-                top: imgtodrag.offset().top,
-                left: imgtodrag.offset().left
-            })
-                .css({
-                'opacity': '0.5',
-                    'position': 'absolute',
-                    'height': '150px',
-                    'width': '150px',
-                    'z-index': '100'
-            })
-                .appendTo($('body'))
-                .animate({
-                'top': cart.offset().top + 10,
-                    'left': cart.offset().left + 10,
-                    'width': 75,
-                    'height': 75
-            }, 1000, 'easeInOutExpo');
 
-            setTimeout(function () {
-                cart.effect("shake", {
-                    times: 2
-                }, 200);
-            }, 1500);
+  var cart = $('.shopping-cart');
+  var imgtodrag = $(this).parent('.item').find("img").eq(0);
+  if (imgtodrag) {
+      var imgclone = imgtodrag.clone()
+          .offset({
+          top: imgtodrag.offset().top,
+          left: imgtodrag.offset().left
+      })
+          .css({
+          'opacity': '0.5',
+              'position': 'absolute',
+              'height': '150px',
+              'width': '150px',
+              'z-index': '100'
+      })
+          .animate({
+          'top': cart.offset().top + 10,
+              'left': cart.offset().left + 10,
+              'width': 75,
+              'height': 75
+      }, 1000, 'easeInOutExpo');
 
-            imgclone.animate({
-                'width': 0,
-                    'height': 0
-            }, function () {
-                $(this).detach()
-            });
-        }
-    });
+      setTimeout(function () {
+          cart.effect("shake", {
+              times: 2
+          }, 200);
+      }, 1500);
+
+      imgclone.animate({
+          'width': 0,
+              'height': 0
+      }, function () {
+          $(this).detach()
+      });
+  }
+
+});
 
 Vue.component("add-to-basket", {
 
@@ -626,6 +626,47 @@ Vue.component("add-to-basket", {
                 quantity: this.quantity,
                 basketItemOrderParams: this.item.properties
             };
+
+
+            var cart = $('.shopping-cart');
+            var imgtodrag = $(this).parent('.item').find("img").eq(0);
+            if (imgtodrag) {
+                var imgclone = imgtodrag.clone()
+                    .offset({
+                    top: imgtodrag.offset().top,
+                    left: imgtodrag.offset().left
+                })
+                    .css({
+                    'opacity': '0.5',
+                        'position': 'absolute',
+                        'height': '150px',
+                        'width': '150px',
+                        'z-index': '100'
+                })
+                    .animate({
+                    'top': cart.offset().top + 10,
+                        'left': cart.offset().left + 10,
+                        'width': 75,
+                        'height': 75
+                }, 1000, 'easeInOutExpo');
+
+                setTimeout(function () {
+                    cart.effect("shake", {
+                        times: 2
+                    }, 200);
+                }, 1500);
+
+                imgclone.animate({
+                    'width': 0,
+                        'height': 0
+                }, function () {
+                    $(this).detach()
+                });
+            }
+
+
+
+
 
             ResourceService.getResource("basketItems").push(basketObject);
 
