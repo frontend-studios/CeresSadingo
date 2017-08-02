7238891d7b880cba750fba595f0166f0a8e2ca61
SadingoDev <m.huch@sadingo.de>
2017-08-02 13:03:16 +0200

    Animations code ausgeblendet

--- resources/js/dist/ceres.js
+++ resources/js/dist/ceres.js
@@ -602,26 +602,26 @@ Vue.component("add-to-basket", {
                         'width': '150px',
                         'z-index': '100'
                 })
-                    .appendTo($('body'))
-                    .animate({
-                    'top': cart.offset().top + 10,
-                        'left': cart.offset().left + 10,
-                        'width': 75,
-                        'height': 75
-                }, 1000, 'easeInOutExpo');
-
-                setTimeout(function () {
-                    cart.effect("shake", {
-                        times: 2
-                    }, 200);
-                }, 1500);
-
-                imgclone.animate({
-                    'width': 0,
-                        'height': 0
-                }, function () {
-                    $(this).detach()
-                });
+                //     .appendTo($('body'))
+                //     .animate({
+                //     'top': cart.offset().top + 10,
+                //         'left': cart.offset().left + 10,
+                //         'width': 75,
+                //         'height': 75
+                // }, 1000, 'easeInOutExpo');
+                //
+                // setTimeout(function () {
+                //     cart.effect("shake", {
+                //         times: 2
+                //     }, 200);
+                // }, 1500);
+                //
+                // imgclone.animate({
+                //     'width': 0,
+                //         'height': 0
+                // }, function () {
+                //     $(this).detach()
+                // });
             }
 
 
