517466ec4f4b0e65cb74581cc3f8e5eea328584b
mhuch <m.huch@sadingo.de>
2017-09-14 22:51:26 +0200

    TopItemsRouteServiceProvider route auf '' gesetzt

--- src/Providers/TopItemsRouteServiceProvider.php
+++ src/Providers/TopItemsRouteServiceProvider.php
@@ -10,6 +10,6 @@ class TopItemsRouteServiceProvider extends RouteServiceProvider
 {
     public function map(Router $router)
     {
-        $router->get('homepage', 'CeresSadingo\Controllers\ContentController@showTopItems');
+        $router->get('', 'CeresSadingo\Controllers\ContentController@showTopItems');
     }
 }
