326e0559b48348ee5bf55956c6882dbb5581af0c
mhuch <m.huch@sadingo.de>
2017-09-23 10:07:56 +0200

    DELETE: Caching

--- src/Providers/CeresSadingoServiceProvider.php
+++ src/Providers/CeresSadingoServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace CeresSadingo\Providers;
 
-use CeresSadingo\Caching\HomepageCacheSettings;
-use CeresSadingo\Caching\NavigationCacheSettings;
-use CeresSadingo\Caching\SideNavigationCacheSettings;
+use Ceres\Caching\HomepageCacheSettings;
+use Ceres\Caching\NavigationCacheSettings;
+use Ceres\Caching\SideNavigationCacheSettings;
 use IO\Extensions\Functions\Partial;
 use IO\Helper\CategoryKey;
 use IO\Helper\CategoryMap;
